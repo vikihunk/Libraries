2d93497d09e564b1943c6f9a69bf7fa00d6324fc
vikihunk <vikihunk@gmail.com>
2018-02-24 13:21:35 +0530

    Creating directory structure for libraries

---
+++ linked_list/ll.h
@@ -0,0 +1,9 @@
+/*
+ * Header file for linked list library
+ * Library maintains a simple linked list
+ * with operations like insert, delete
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINKS	20
